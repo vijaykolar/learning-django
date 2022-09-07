a02ba2144e5a18d5c17f2c0e2237c37ef7e4d7a3
vijaykolar <vijayikolar@gmail.com>
2022-09-07 16:52:45 +0530

    Worki ng on posts app

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
