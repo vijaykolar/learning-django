955dfe8eae4519a2f3c8db7728ce3515e20660e0
vijaykolar <vijayikolar@gmail.com>
2022-09-07 17:48:42 +0530

    Added posts model

--- test_project/posts/models.py
+++ test_project/posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    text = models.TextField()
+    id = models.IntegerField(primary_key=True)
+
+    def __str__(self):
+        return self.text
