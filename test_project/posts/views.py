4b357d5d6f708d04640e4d5e4c81d345630ed47e
vijaykolar <vijayikolar@gmail.com>
2022-09-07 18:14:33 +0530

    Completed message board app

--- test_project/posts/views.py
+++ test_project/posts/views.py
@@ -1,3 +1,16 @@
+import imp
 from django.shortcuts import render
+from django.views.generic import ListView, TemplateView
+from .models import Post
+# Create your views here.
 
+
+class HomePageView(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'all_posts_list'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
 # Create your views here.
